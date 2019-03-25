8a86c1ed1aa46fa3bf93948401042c7f3f2946ca
Aleksandra <a.fulmanski@gmail.com>
2019-03-25 16:39:46 +0100

    Routage

--- public/index.php
+++ public/index.php
@@ -10,9 +10,28 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 $app = new App();
 
-$app->get('/hello', function ( ServerRequestInterface $request, ResponseInterface $response, ?array $args
+$route = $app->get('/', function ( ServerRequestInterface $request, ResponseInterface $response, ?array $args
 ) {
     return $response->getBody()->write('<h1>Hello</h1>');
 });
+$route->setName('homepage');
+
+
+$app->group('/projet', function () {
+    $this->get("/{id:\d+}", function ( ServerRequestInterface $request, ResponseInterface $response, ?array $args
+    ) {
+        return $response->getBody()->write('<h1>Détail du projet</h1>');
+    }) ->setName('app_project_show');
+
+    $this->get("/creation", function ( ServerRequestInterface $request, ResponseInterface $response, ?array $args
+    ) {
+        return $response->getBody()->write('<h1>Création d\'un projet</h1>');
+    }) ->setName('app_project_create');
+});
+
+
+
+
+
 //création et renvoi de la réponse au navigateur
 $app->run();
