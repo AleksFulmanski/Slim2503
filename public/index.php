e5818bb2d2a4c009be195986b68e331c33989633
Aleksandra <a.fulmanski@gmail.com>
2019-03-25 14:52:37 +0100

    htaccess et créa première page

--- public/index.php
+++ public/index.php
@@ -1,3 +1,18 @@
 <?php
-echo 'Hello'
-?>
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\App;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+//création de l'application
+
+$app = new App();
+
+$app->get('/hello', function ( ServerRequestInterface $request, ResponseInterface $response, ?array $args
+) {
+    return $response->getBody()->write('<h1>Hello</h1>');
+});
+//création et renvoi de la réponse au navigateur
+$app->run();
