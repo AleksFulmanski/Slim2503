49192b27095fccb65bbbae4e1615aab9199cc310
Aleksandra <a.fulmanski@gmail.com>
2019-03-27 11:44:46 +0100

    reorganisation projet

--- config/container.php
+++ config/container.php
@@ -1,7 +1,47 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: stagiaire
- * Date: 27/03/2019
- * Time: 11:20
- */
+
+use App\Controller\AboutController;
+use App\Controller\ContactController;
+use App\Controller\ProjectController;
+use Psr\Container\ContainerInterface;
+
+
+
+//récupération du conteneur grace à App
+$container = $app->getContainer();
+
+
+//configuration de Twig
+
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig(dirname(__DIR__) . '/templates', [
+        'cache' => false
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $router = $container->get('router');
+    $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
+    $view->addExtension(new Slim\Views\TwigExtension($router, $uri));
+
+    return $view;
+};
+
+// Création des clefs
+
+//on crée une nouvelle clé ProjectController pour dire au container comment instancier
+//ProjectController  (recuperer le Twig défini dans la function
+//construct dans ProjectController
+//il retourne une nouvelle instance de ProjectControlleur
+//on obtient twig en envoyant la clef view du conteneur
+
+$container[ProjectController::class] = function ($container) {
+    return new ProjectController($container->get('view'));
+};
+
+$container[ContactController::class] = function (ContainerInterface $container) {
+    return new ContactController($container->get('view'));
+};
+
+$container[AboutController::class] = function ($container) {
+    return new AboutController($container->get('view'));
+};
--- config/routes.php
+++ config/routes.php
@@ -5,3 +5,44 @@
  * Date: 27/03/2019
  * Time: 11:21
  */
+
+//création des routes
+
+use App\Controller\AboutController;
+use App\Controller\ContactController;
+use App\Controller\ProjectController;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+
+//page d'accueil
+$route = $app->get('/', function (ServerRequestInterface $request, ResponseInterface $response, ?array $args
+) {
+    //return $response->getBody()->write('<h1>Hello</h1>');
+    return $this->view->render($response, 'home.twig');
+});
+
+
+
+
+$route->setName('homepage');
+
+//groupe des routes du projet
+
+$app->group('/projet', function () {
+
+    //Page de création
+    $this->get("/creation", ProjectController::class . ':create')->setName('app_project_create');
+
+    // création d'une page de détail des projets
+    //Nouveauté : on ajoute une variable dans l'URL avec des accolades
+    $this->get("/{id:\d+}", ProjectController::class . ':show')->setName('app_project_show');
+
+});
+
+$app->get('/contact', ContactController::class . ':contact')->setName('app_contact');
+
+$app->get('/about', AboutController::class . ':about')->setName('app_about');
+
+
+
